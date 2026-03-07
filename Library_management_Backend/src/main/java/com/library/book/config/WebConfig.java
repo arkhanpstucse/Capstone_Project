ab14a8f366606af8b44b56fd9c63796bde16bd9f
arkhanpstucse <abdur.rahman.khan.01502@gmail.com>
2026-03-07 15:31:12 +0600

    final

--- Library_management_Backend/src/main/java/com/library/book/config/WebConfig.java
+++ Library_management_Backend/src/main/java/com/library/book/config/WebConfig.java
@@ -10,7 +10,10 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins(
+                        "http://localhost:3000",
+                        "http://54.152.39.92"
+                )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
